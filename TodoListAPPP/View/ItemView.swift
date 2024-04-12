cee02907d9ac246ef004dbb17c279c3d9459bce5
Pablo Alvarado <palvarado@lcs.on.ca>
2024-04-11 20:52:23 -0400

    Done with Item view.

--- TodoListAPPP/View/ItemView.swift
+++ TodoListAPPP/View/ItemView.swift
@@ -12,10 +12,46 @@ struct ItemView: View {
     let currentItem: TodoItem
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Label(
+
+                    title: {
+
+                        Text(
+                            currentItem.title
+                        )
+                        
+                    },
+                    icon: {
+                        
+                        Image(
+                            systemName: currentItem.done == true ? "checkmark.circle" : "circle"
+                        )
+
+                    }
+
+                )
     }
 }
 
 #Preview {
-    ItemView()
-}
+    List{
+
+        ItemView(
+            currentItem: firstItem
+        )
+        
+        ItemView(
+            currentItem: secondItem
+        )
+        
+        ItemView(
+            currentItem: thirdItem
+        )
+
+
+
+        }
+
+        
+
+    }
