cbbbf7e286202fe2fe6a778adaf6f105bda6bc9a
Pablo Alvarado <palvarado@lcs.on.ca>
2024-04-11 20:09:37 -0400

    Done with TodoItem

--- TodoListAPPP/Model/TodoItem.swift
+++ TodoListAPPP/Model/TodoItem.swift
@@ -12,3 +12,19 @@ struct TodoItem: Identifiable {
     var title: String
     var done: Bool
 }
+
+let firstItem = TodoItem(title: "study for Chemistry quiz", done: false)
+
+    let secondItem = TodoItem(title: "Finish Computer Science assignment", done: true)
+
+    let thirdItem = TodoItem(title: "Go for a run around campus", done: false)
+
+let exampleItems = [
+    
+   firstItem
+   ,
+   secondItem
+   ,
+   thirdItem
+
+]
