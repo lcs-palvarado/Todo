25f1476f69e06d043f9c5f9d5b1003c370a3d746
Pablo Alvarado <palvarado@lcs.on.ca>
2024-04-13 10:49:24 -0400

    Added model container to the app entry point.

--- TodoListAPPP/TodoListAPPPApp.swift
+++ TodoListAPPP/TodoListAPPPApp.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Pablo Alvarado on 11/04/24.
 //
-
+import SwiftData
 import SwiftUI
 
 @main
@@ -12,6 +12,7 @@ struct TodoListAPPPApp: App {
     var body: some Scene {
         WindowGroup {
             LandingView()
+                .modelContainer(for: TodoItem.self)
         }
     }
 }
